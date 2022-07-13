44326a8af5dd628ed977e87e7da934d20b12adec
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-07-13 21:48:08 +0300

    commit tree e123a6793092214635053bf456710f2348d38973

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function handleClick(event){
+        console.log(event)
+    }
+    return(
+        <div>
+            
+            <button onClick={handleClick}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,27 @@
 // Code Keypad Component Here
+
+import React from "react";
+
+function Keypad(){
+    function handleChange(event){
+        console.log(`${event.target.name}`)
+    }
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log("submit")
+    }
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+            <input 
+            type="password" 
+            name="Entering password..."
+            placeholder="Entering password..."
+            onChange = {handleChange} />
+            <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Keypad;
